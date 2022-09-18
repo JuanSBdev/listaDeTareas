ccf74ad84f6c6c2a2bc7d68bed70f0ec507ece36
Juan SB <juansb.dev@gmail.com>
2022-09-18 20:04:35 -0300

    finalizada

---
+++ script.js
@@ -0,0 +1,68 @@
+   /* console.log( "ready!" );
+    var lista = document.getElementById("lista");
+	var	tareaInput = document.getElementById("input-uno");
+	var	btnNuevaTarea = document.getElementById("boton-add");
+   const agregarTarea = function (){
+    console.log('asd')
+        var tarea = tareaInput.value,
+        	li = document.createElement('li'),
+			a = document.createElement('a'),
+        	enlace = document.createTextNode(tarea);
+			a.appendChild(enlace),
+			li.appendChild(a)
+		if (tarea == "") {
+			alert("You must write something!");
+
+		  } else {
+			document.getElementById("lista").appendChild(li);
+			tareaInput.value = "";
+		}        
+	
+    };
+  
+	*/
+
+	const input = document.querySelector('input');
+	const addBtn = document.querySelector('.btn-add');
+	const ul = document.querySelector('ul');
+	const empty = document.querySelector('.empty');
+	addBtn.addEventListener('click', (e) => {
+		
+			e.preventDefault();
+			const text = input.value;
+				if( text != ""){
+					const li = document.createElement('li');
+					const p = document.createElement('p');
+					p.textContent = text;
+					li.appendChild(p);
+					li.appendChild(addDeleteBtn())
+			
+					ul.appendChild(li);
+			
+					input.value = "";
+					empty.style.display = "none";
+				}
+				else{
+					console.log('none')
+				}
+
+			
+		
+	});
+	function addDeleteBtn(){
+		const deleteBtn = document.createElement('button');
+		deleteBtn.textContent = 'X';
+		deleteBtn.className = "btn-delete"
+
+		deleteBtn.addEventListener('click', (e)=>{
+			const item = e.target.parentElement;
+			ul.removeChild(item);
+
+			const items = document.querySelectorAll('li');
+			if(items.length == 0){
+				empty.style.display = "block";
+			}
+			
+		});
+		return deleteBtn;
+	}

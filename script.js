6d3b1ac568a2915f3022aa6abe48d55fb3c4081d
Juan SB <juansb.dev@gmail.com>
2023-02-27 12:58:26 -0300

    details

--- script.js
+++ script.js
@@ -1,80 +1,4 @@
-   /* console.log( "ready!" );
-    var lista = document.getElementById("lista");
-	var	tareaInput = document.getElementById("input-uno");
-	var	btnNuevaTarea = document.getElementById("boton-add");
-   const agregarTarea = function (){
-    console.log('asd')
-        var tarea = tareaInput.value,
-        	li = document.createElement('li'),
-			a = document.createElement('a'),
-        	enlace = document.createTextNode(tarea);
-			a.appendChild(enlace),
-			li.appendChild(a)
-		if (tarea == "") {
-			alert("You must write something!");
 
-		  } else {
-			document.getElementById("lista").appendChild(li);
-			tareaInput.value = "";
-		}        
-	
-    };
-  
-	
-	const input = document.querySelector('input');
-	const addBtn = document.querySelector('.btn-add');
-	const ul = document.querySelector('ul');
-	const empty = document.querySelector('.empty');
-	addBtn.addEventListener('click', (e) => {
-		e.preventDefault();
-		const text = input.value;
-		
-		if( text != ""){
-			
-			const li = document.createElement('li');
-			const p = document.createElement('p');
-			p.textContent = text;
-			localStorage.setItem('tareas', text);
-			
-			li.appendChild(p);
-			li.appendChild(addDeleteBtn())
-			
-			ul.appendChild(li);
-			localStorage.setItem('tare', li);
-			
-			input.value = "";
-			empty.style.display = "none";
-			console.log();
-		}
-		else{
-			
-			console.log(localStorage.getItem('tareas'));
-			
-			
-		}
-		
-		
-		
-	});
-	
-	function addDeleteBtn(){
-		const deleteBtn = document.createElement('button');
-		deleteBtn.textContent = 'X';
-		deleteBtn.className = "btn-delete"
-		
-		deleteBtn.addEventListener('click', (e)=>{
-			const item = e.target.parentElement;
-			ul.removeChild(item);
-			
-			const items = document.querySelectorAll('li');
-			if(items.length == 0){
-				empty.style.display = "block";
-			}
-			
-		});
-		return deleteBtn;
-	}
-	*/
 	const input = document.querySelector('.input-btn input');
 const listTasks = document.querySelector('.list-tasks ul');
 const message = document.querySelector('.list-tasks');
@@ -106,7 +30,7 @@ function deleteAll(){
 function addTasks(){
     const task = input.value;
     if (task === '') {
-        showError('The field is empty...');
+        showError('Nothing to do?..');
         return;
     }
 
